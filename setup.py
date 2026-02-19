a97c39dfa1b14ea5d297e9b280dff606c0f15e6e
mohammadaltamash5203-tech <mohammadaltamash5203@gmail.com>
2026-02-19 21:54:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
+import sys
 from setuptools import setup, find_packages
 
+# Check if it's Termux or Android
+is_mobile = "android" in sys.platform.lower() or "termux" in sys.executable
+
+reqs = ['ujson', 'aiohttp']
+if not is_mobile:
+    reqs.append('uvloop') # VPS par uvloop install hoga, Termux par skip
+
 setup(
-    name="d4x_turbo",
-    version="1.0",
+    name="d4x-turbo",
+    version="1.3",
     packages=find_packages(),
-    install_requires=['ujson', 'uvloop; platform_system != "Windows"'],
+    install_requires=reqs,
+    # Baaki info...
 )
